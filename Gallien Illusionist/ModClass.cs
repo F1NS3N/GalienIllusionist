b7f70399a7a3cc216a25df8f9da58e1c4609d8df
FIN <kyzmindimon1311@gmail.com>
2025-07-19 16:51:14 +0300

    Galien Illusionist

---
+++ Gallien Illusionist/ModClass.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using HutongGames.PlayMaker;
+using HutongGames.PlayMaker.Actions;
+using Modding;
+using UnityEngine;
+using UnityEngine.UIElements;
+using Vasi;
+
+namespace GalienIllusionist
+{
+    public class GalienIllusionist : Mod
+    {
+        public GalienIllusionist() : base("GalienIllusionist") { }
+
+        public override string GetVersion() => "v1.0.0";
+
+        public override void Initialize()
+        {
+            On.PlayMakerFSM.Start += PlayMakerFSM_Galien;
+            On.HutongGames.PlayMaker.Actions.CreateObject.OnEnter += CreateObject_OnEnter;
+        }
+        private void CreateObject_OnEnter(On.HutongGames.PlayMaker.Actions.CreateObject.orig_OnEnter orig, HutongGames.PlayMaker.Actions.CreateObject self)
+        {
+            orig(self);
+
+            if (self.Fsm.GameObject.name == "Ghost Warrior Galien" &&
+            self.Fsm.Name == "Summon Minis" &&
+            (self.State.Name == "Copy Summon" || self.State.Name == "Copy Summon 2"))
+            {
+                GameObject sphere = self.storeObject.Value;
+                sphere.GetComponent<DamageHero>().damageDealt = 0;
+            }
+        }
+        public void PlayMakerFSM_Galien(On.PlayMakerFSM.orig_Start orig, PlayMakerFSM self)
+        {
+            orig(self);
+
+            if (self.gameObject.name == "Ghost Warrior Galien" && self.FsmName == "Summon Minis")
+            {
+                self.CopyState("Summon", "Copy Summon");
+                self.ChangeTransition("Summon", "FINISHED", "Copy Summon");
+                self.ChangeTransition("Copy Summon", "FINISHED", "Idle 2");
+
+                self.CopyState("Summon 2", "Copy Summon 2");
+                self.GetState("Summon 2").AddTransition("FINISHED", "Copy Summon 2");
+                self.ChangeTransition("Copy Summon 2", "FINISHED", "Summon 2");
+            }
+        }
+    }
+}
---
+++ Gallien Illusionist/ReadMe.md
@@ -0,0 +1,3 @@
+# Gallien_Illusionist
+
+A mod for the game Hollow Knight.
