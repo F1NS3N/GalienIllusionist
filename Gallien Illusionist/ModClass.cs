fcf3689a9773070007404cc2b915a26d65d061cc
FIN <kyzmindimon1311@gmail.com>
2025-07-20 10:46:47 +0300

    fix

--- Gallien Illusionist/ModClass.cs
+++ Gallien Illusionist/ModClass.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using HutongGames.PlayMaker;
 using HutongGames.PlayMaker.Actions;
 using Modding;
 using UnityEngine;
-using UnityEngine.UIElements;
 using Vasi;
 
 namespace GalienIllusionist
@@ -14,6 +10,7 @@ namespace GalienIllusionist
     public class GalienIllusionist : Mod
     {
         private static List<SpriteRenderer> SpritesToFade = new List<SpriteRenderer>();
+
         public GalienIllusionist() : base("GalienIllusionist") { }
 
         public override string GetVersion() => "v1.0.1";
@@ -40,11 +37,9 @@ namespace GalienIllusionist
                 Color c = sr.color;
                 if (c.a < 1f)
                 {
-                    Task.Delay(100).ContinueWith(_x =>
-                    {
-                        c.a += 0.01f;
-                        sr.color = c;
-                    });
+                    // Плавное появление за 5 секунд
+                    c.a += Time.deltaTime / 5f;
+                    sr.color = c;
                 }
                 else
                 {
@@ -52,26 +47,38 @@ namespace GalienIllusionist
                 }
             }
         }
+
         private void CreateObject_OnEnter(On.HutongGames.PlayMaker.Actions.CreateObject.orig_OnEnter orig, HutongGames.PlayMaker.Actions.CreateObject self)
         {
             orig(self);
 
             if (self.Fsm.GameObject.name == "Ghost Warrior Galien" &&
-            self.Fsm.Name == "Summon Minis" &&
-            (self.State.Name == "Copy Summon" || self.State.Name == "Copy Summon 2"))
+                self.Fsm.Name == "Summon Minis" &&
+                (self.State.Name == "Copy Summon" || self.State.Name == "Copy Summon 2"))
             {
                 // Копия не наносит урона
                 GameObject sphere = self.storeObject.Value;
-                sphere.GetComponent<DamageHero>().damageDealt = 0;
+                if (sphere == null) return;
+
+                DamageHero damageHero = sphere.GetComponent<DamageHero>();
+                if (damageHero != null)
+                {
+                    damageHero.damageDealt = 0;
+                }
 
                 // Находим спрайт и делаем его прозрачным
                 SpriteRenderer transparency = sphere.GetComponentInChildren<SpriteRenderer>();
-                Color newColor = transparency.color;
-                newColor.a = 0.4f;
-                transparency.color = newColor;
-                SpritesToFade.Add(transparency);
+                if (transparency != null)
+                {
+                    Color newColor = transparency.color;
+                    newColor.a = 0.4f; // Начинаем с полной прозрачности
+                    transparency.color = newColor;
+
+                    SpritesToFade.Add(transparency);
+                }
             }
         }
+
         public void PlayMakerFSM_Galien(On.PlayMakerFSM.orig_Start orig, PlayMakerFSM self)
         {
             orig(self);
@@ -80,32 +87,44 @@ namespace GalienIllusionist
             {
                 // Первая сфера
                 self.CopyState("Summon", "Copy Summon");
-                
-                // Изменение позиции первой сферы
-                CreateObject createAction = self.GetState("Copy Summon").GetAction<CreateObject>();
-                if (createAction != null)
-                {
-                    Vector3 galienPosition = self.transform.position;
-                    createAction.position.Value = new Vector3(3f, 0f, 0.029f);
-                }
 
-                self.ChangeTransition("Summon", "FINISHED", "Copy Summon");
-                self.ChangeTransition("Copy Summon", "FINISHED", "Idle 2");
+                FsmState copySummonState = self.GetState("Copy Summon");
+                if (copySummonState != null)
+                {
+                    CreateObject createAction = copySummonState.GetAction<CreateObject>();
+                    if (createAction != null)
+                    {
+                        createAction.position.Value = new Vector3(3f, 0f, 0.029f);
+                    }
+
+                    if (self.GetState("Summon") != null)
+                    {
+                        self.ChangeTransition("Summon", "FINISHED", "Copy Summon");
+                    }
+
+                    self.ChangeTransition("Copy Summon", "FINISHED", "Idle 2");
+                }
 
                 // Вторая сфера
                 self.CopyState("Summon 2", "Copy Summon 2");
-                createAction = self.GetState("Copy Summon 2").GetAction<CreateObject>();
-                if (createAction != null)
-                {
 
-                    Vector3 galienPosition = self.transform.position;
-                    createAction.position.Value = new Vector3(3f, 0f, 0.029f);
+                FsmState copySummon2State = self.GetState("Copy Summon 2");
+                if (copySummon2State != null)
+                {
+                    CreateObject createAction2 = copySummon2State.GetAction<CreateObject>();
+                    if (createAction2 != null)
+                    {
+                        createAction2.position.Value = new Vector3(3f, 0f, 0.029f);
+                    }
+
+                    if (self.GetState("Summon 2") != null)
+                    {
+                        self.GetState("Summon 2").AddTransition("FINISHED", "Copy Summon 2");
+                    }
+
+                    self.ChangeTransition("Copy Summon 2", "FINISHED", "Summon 2");
                 }
-                self.GetState("Summon 2").AddTransition("FINISHED", "Copy Summon 2");
-                self.ChangeTransition("Copy Summon 2", "FINISHED", "Summon 2");
             }
         }
     }
 }
-
-// Ино гей
